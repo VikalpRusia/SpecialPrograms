cdedbf898b21d1a238edcd8f4abb9625a038c182
VikalpRusia <53312141+VikalpRusia@users.noreply.github.com>
2020-06-28 01:47:29 +0530

    Rename to Example added Some links to question

--- Example.java
+++ Example.java
@@ -1,3 +1,5 @@
+// Numbers of length N and value less than K
+// https://www.interviewbit.com/problems/numbers-of-length-n-and-value-less-than-k/#
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
